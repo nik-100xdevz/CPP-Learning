c31b1f534db26f6673edbddcaee7f9e94006d95d
NIKHIL_RAI <rainikhil760@gmail.com>
2025-02-09 18:52:50 +0530

    Add files via upload

---
+++ cpp-question solving/codechef/01-i/o.cpp
@@ -0,0 +1,27 @@
+// Chef and Instant Noodles
+// Chef has invented 
+// 1
+// 1-minute Instant Noodles. As the name suggests, each packet takes exactly 
+// 1
+// 1 minute to cook.
+
+// Chef's restaurant has 
+// X
+// X stoves and only 
+// 1
+// 1 packet can be cooked in a single stove at any minute.
+
+// How many customers can Chef serve in 
+// Y
+// Y minutes if each customer orders exactly 
+// 1
+// 1 packet of noodles?
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	cout<<X*Y;
+}
